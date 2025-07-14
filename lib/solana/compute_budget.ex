d13326de8b79303a78a54f69c13aa9cf0365ef28
Michal Strzelczyk <817457+panmichal@users.noreply.github.com>
2025-07-14 08:43:02 +0200

    Add setComputeUnitPrice instruction (#14)

--- lib/solana/compute_budget.ex
+++ lib/solana/compute_budget.ex
@@ -11,6 +11,14 @@ defmodule Solana.ComputeBudget do
     ]
   ]
 
+  @set_compute_unit_price_schema [
+    price: [
+      type: :pos_integer,
+      required: true,
+      doc: "Compute unit price in micro lamports"
+    ]
+  ]
+
   def id, do: Solana.pubkey!("ComputeBudget111111111111111111111111111111")
 
   def set_compute_unit_limit(opts) do
@@ -19,6 +27,19 @@ defmodule Solana.ComputeBudget do
         %Instruction{
           program: id(),
           data: Instruction.encode_data([2, {params.limit, 32}])
+        }
+
+      error ->
+        error
+    end
+  end
+
+  def set_compute_unit_price(opts) do
+    case validate(opts, @set_compute_unit_price_schema) do
+      {:ok, params} ->
+        %Instruction{
+          program: id(),
+          data: Instruction.encode_data([3, {params.price, 64}])
         }
 
       error ->
